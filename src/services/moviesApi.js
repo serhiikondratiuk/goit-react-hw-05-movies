c0ff9873dacb59e50509dce7022dbe577c20ef96
Serhii Kondratiuk <sergey932015@gmail.com>
2022-01-16 15:33:38 +0200

    add movie-details-view

--- src/services/moviesApi.js
+++ src/services/moviesApi.js
@@ -1,11 +1,5 @@
-// https://api.themoviedb.org/3/movie/550?api_key=c89f22547ec952f04456d3
-
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = "c89f22547ec952f04456d39efb9827a2";
-
-// fetch(`${BASE_URL}movie/550?api_key=${API_KEY}`).then((response) =>
-//   console.log(response)
-// );
 
 async function fetchWithErrorHandling(url) {
   const response = await fetch(url);
@@ -15,5 +9,29 @@ async function fetchWithErrorHandling(url) {
 export function fetchTrendingMovies() {
   return fetchWithErrorHandling(
     `${BASE_URL}trending/all/day?api_key=${API_KEY}`
+  );
+}
+
+export function fetchMovieByName(query, page) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}&include_adult=false`
+  );
+}
+
+export function fetchMovieById(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+}
+
+export function fetchMovieCast(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+}
+
+export function fetchMovieReviews(movieId) {
+  return fetchWithErrorHandling(
+    `${BASE_URL}movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US`
   );
 }
--- src/views/MovieDetails/MovieDetailsPage.js
+++ src/views/MovieDetails/MovieDetailsPage.js
@@ -1,9 +1,52 @@
+import s from "./MovieDetails.module.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import * as moviesAPI from "../../services/moviesApi";
 
-function MovieDetailsPage(id) {
-  const params = useParams();
-  return <h2>MovieDetails</h2>;
+function MovieDetailsPage() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    moviesAPI.fetchMovieById(movieId).then(setMovie);
+  }, [movieId]);
+
+  return (
+    <>
+      {movie && (
+        <div className={s.wrapper}>
+          <h2 className={s.title}>{movie.title}</h2>
+          <div className={s.flexWrapper}>
+            <img
+              className={s.img}
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div className={s.textWrapper}>
+              <p className={s.overview}>
+                <span className={s.span}>Overview:</span> {movie.overview}
+              </p>
+              <p className={s.releaseDate}>
+                <span className={s.span}>Release date: </span>
+                {movie.release_date}
+              </p>
+              <p className={s.raiting}>
+                <span className={s.span}>Raiting: </span> {movie.vote_average}
+              </p>
+              <ul className={s.genresList}>
+                <span className={s.span}>Genres:</span>
+                {movie.genres.map((genre) => (
+                  <li className={s.genre} key={genre.id}>
+                    {genre.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default MovieDetailsPage;
